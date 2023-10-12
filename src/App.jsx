a5f58a4bf87552fd87bdbf36859b4a02f301dc8b
Ayeza <ayezabashir442@gmail.com>
2023-10-12 20:11:39 +0500

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './components/header/Header'
 
 function App() {
 
   return (
     <>
-      hello world
+      <Header />
     </>
   )
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import logo from '/headerlogo.png'
+const Header = () => {
+    return (
+        <>
+            <header className='header-logo'>
+                <img src={logo} alt="" className='logo' />
+            </header>
+        </>
+    )
+}
+
+export default Header
